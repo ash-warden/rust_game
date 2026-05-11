5377978caa6b6c837aed75daa7b2122928f6e39f
ash <ashwarden@outlook.com.au>
2026-05-11 18:52:51 +1000

    made save menu simpler, and removed the rfd dependency. need to fix some weird issues with the saving/loading (collected items are getting lost)

--- src/area.rs
+++ src/area.rs
@@ -23,7 +23,7 @@ impl Area {
     }
 
     pub fn get_obj_for_room(&self, room_x: i32, room_y: i32) -> Option<&Vec<Arc<dyn Obj>>> {
-        println!("{}_{}", room_x, room_y);
+        // println!("{}_{}", room_x, room_y);
         self.objects.objects.get(&format!("{}_{}", room_x, room_y))
     }
 }
